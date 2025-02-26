432387ad0d6d9bdf0be92e60f33fbf1f7f215a86
Seshagiri Sriram <SeshagiriSriram@gmail.com>
2025-02-26 09:20:43 +0530

    Changes to be committed: 	modified:   User.java
    Removed getter and setter since using Lombok fully.

--- simplejsonreadwrite-jackson/src/main/java/org/sriram/demo/User.java
+++ simplejsonreadwrite-jackson/src/main/java/org/sriram/demo/User.java
@@ -13,18 +13,6 @@ public class User implements Serializable {
 	private String state; 
 	private String country; 
 
-	public String getName() { return name; } 
-	public int getAge() { return age; } 
-	public String getCity() { return city; } 
-	public String getState() { return state; } 
-	public String getCountry() { return country; } 
-	
-	public void setName(String name) { this.name = name; } 
-	public void setAge(int age) { this.age = age; } 
-	public void setCity(String city) { this.city = city; } 
-	public void setState(String state) { this.state = state; } 
-	public void setCountry (String country) { this.country = country; } 
-	
 	public String toString() { 
 		return "{name: \""+ getName() +"\", age:" + getAge() + ", city: \""+ getCity() + "\",state: \""+ getState() + "\", country: \"" +getCountry() + "\"}"; 
 	}
